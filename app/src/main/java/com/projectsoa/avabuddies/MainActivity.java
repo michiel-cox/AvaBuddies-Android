08ce79dbb852491bc5a9d77fa4c9cdd68bdb336c
Michiel Cox <michielcox12@gmail.com>
2019-03-18 12:19:13 +0100

    Initial commit

---
+++ app/src/main/java/com/projectsoa/avabuddies/MainActivity.java
@@ -0,0 +1,14 @@
+package com.projectsoa.avabuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
