5bc887d8e28f0e2d54cf3215e1e397f4f0aec3e3
Michiel <michielcox12@gmail.com>
2019-03-20 20:10:59 +0100

    Added libraries

--- app/src/main/java/com/projectsoa/avabuddies/BaseActivity.java
+++ app/src/main/java/com/projectsoa/avabuddies/BaseActivity.java
@@ -1,21 +1,33 @@
 package com.projectsoa.avabuddies;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
 import org.greenrobot.eventbus.EventBus;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import dagger.android.support.DaggerAppCompatActivity;
 
-public abstract class BaseActivity  extends AppCompatActivity {
+public abstract class BaseActivity  extends DaggerAppCompatActivity {
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        EventBus.getDefault().unregister(this);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onStart() {
+        super.onStart();
         EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 
 }
