69e5c23456a06b66426daf39be2e48e0ef2fe4f4
Michiel <michielcox12@gmail.com>
2019-04-11 14:18:20 +0200

    QR Read & Show initial commit

---
+++ app/src/main/java/com/projectsoa/avabuddies/screens/main/qrread/QRReadFragment.java
@@ -0,0 +1,20 @@
+package com.projectsoa.avabuddies.screens.main.qrread;
+
+import com.projectsoa.avabuddies.R;
+import com.projectsoa.avabuddies.core.base.BaseFragment;
+
+
+public class QRReadFragment extends BaseFragment {
+
+
+    public QRReadFragment() {
+
+    }
+
+
+    @Override
+    protected int layoutRes() {
+        return R.layout.fragment_qrread;
+    }
+
+}
---
+++ app/src/main/java/com/projectsoa/avabuddies/screens/main/qrshow/QRShowFragment.java
@@ -0,0 +1,25 @@
+package com.projectsoa.avabuddies.screens.main.qrshow;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.projectsoa.avabuddies.R;
+import com.projectsoa.avabuddies.core.base.BaseFragment;
+
+public class QRShowFragment extends BaseFragment {
+
+
+    public QRShowFragment() {
+
+    }
+
+
+    @Override
+    protected int layoutRes() {
+        return R.layout.fragment_qrshow;
+    }
+}
